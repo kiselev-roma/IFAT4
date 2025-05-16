bea7c44d83370ad76f5a4c7169437a9af3878da7
kiselev-roma <Reebok7991@mail.ru>
2025-05-16 15:46:36 +0400

    First test

---
+++ src/test/java/Lokator.java
@@ -0,0 +1,22 @@
+public class Lokator {
+    // http://testingchallenges.thetestingmap.org/
+
+    // ----FirstName----
+    // id: firstname  (css: #firstname)
+    // name: firstname
+
+    // ----Country----
+    // class: inputbox  (css: .inputbox)
+    // tag: //input
+
+    // ----Email----
+    // text: //span[text()='Email']             - строгое соответствие поиска
+    // text: //span[contains(text(),'Email')]   - ищем span, в котором есть слово Email
+
+    // ----Lastname----
+    // css: input[value=Gheorghe]
+
+    // ----Submit----
+    // xpath: //input[@value='Submit']
+
+}
